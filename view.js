d2229cb4e7fd9e2e352dad4387887e436321e861
Edwin Galeano <ed.galeano1@gmail.com>
2023-05-07 00:50:48 -0500

    initial

--- view.js
+++ view.js
@@ -1,7 +1,61 @@
 class generalViewController {
-  constructor() {
+  constructor () {
     // primary attributes
-    this.widthList = [];
+    // related to attribute alt for img 
+    this.popUpObject = {
+      app_logo: {
+        account: "Cuenta de Google",
+        username: "Edwin Galeano",
+        email: "ed.galeano1@gmail.com",
+      },
+      profile_image: "Google Apps",
+    };
+    // obtain html element
+    this.elementsWithPopUp = document.querySelectorAll(".pop-up-container");
+    this.elementsWithPopUp.forEach((el, index) => {
+      for (const e in this.popUpObject) {
+        const imgNode = this.getImageChildNode(el.childNodes);
+        const ulNode = this.getUlChildNode(el.childNodes);
+        if (imgNode.alt === e) {
+          if (typeof this.popUpObject[e] === 'string') {
+            const newLi = document.createElement('li');
+            newLi.textContent = this.popUpObject[e];
+            ulNode.appendChild(newLi);
+          }
+          else {
+            const keys = Object.keys(this.popUpObject[e]).length;
+            for (let i=0; i<keys; i++) {
+              const newLi = document.createElement('li');
+              let text = "";
+              if (i === 0) {
+                text = document.createElement('b');
+                text.textContent = this.popUpObject[e]["account"];
+              }
+              else if (i === 1) newLi.textContent = this.popUpObject[e]["username"]; 
+              else newLi.textContent = this.popUpObject[e]["email"];
+              if (i !== 0) ulNode.appendChild(newLi);
+              else ulNode.appendChild(text);
+            }
+          }
+        }
+      };
+    });
+  }
+
+  getImageChildNode (nList) {
+    let childNod = {};
+    nList.forEach(el => {
+      if (el.nodeName === "IMG") childNod = el;
+    });
+    return childNod;
+  }
+
+  getUlChildNode (nList) {
+    let childNod = {};
+    nList.forEach(el => {
+      if (el.nodeName === "UL") childNod = el;
+    });
+    return childNod;
   }
 }
 
